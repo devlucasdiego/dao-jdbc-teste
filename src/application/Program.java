29e17d02e37deb6362f9214b4dbd2c8fe039517b
Lucas Diego <luucasdiego_12@hotmail.com>
2021-11-19 10:30:38 -0400

    findById

--- src/application/Program.java
+++ src/application/Program.java
@@ -10,12 +10,10 @@ import model.entities.Seller;
 public class Program {
 
 	public static void main(String[] args) {
-		Department obj = new Department(1, "Books");
-		System.out.println(obj);
-
-		Seller seller = new Seller(21, "Lucas", "lucas@gmail.com", new Date(), 3000.0, obj);
 
 		SellerDao sellerDao = DaoFactory.createSellerDao();
+
+		Seller seller = sellerDao.findById(7);
 
 		System.out.println(seller);
 	}
